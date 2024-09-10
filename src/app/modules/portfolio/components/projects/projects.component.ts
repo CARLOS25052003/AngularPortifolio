602573d0f23876c037c51e0f4d1305829feea226
Carlos Charles <carloscharles016@outlook.com>
2024-09-10 08:32:48 -0400

    finalizando o projeto

--- src/app/modules/portfolio/components/projects/projects.component.ts
+++ src/app/modules/portfolio/components/projects/projects.component.ts
@@ -1,23 +1,29 @@
-import {Component, signal} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {IProjects} from "../../interface/IProjects.interface";
+
+import {MatDialog, MatDialogModule} from '@angular/material/dialog';
+import {EDialogPanelClass} from "../../enum/EDialogPanelClass.enum";
+import {DialogProjectsComponent} from "../dialog/dialog-projects/dialog-projects.component";
+
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
   public arrayProjects = signal<IProjects[]>([
     {
-      src: 'assets/imgs/fotominha.jpeg',
+      src: 'Em Breve',
       alt: 'Primeiro projeto',
-      title:"Primeiro projeto",
+      title: "Primeiro projeto em andamento",
       with: '100px',
-      height:'51px',
+      height: '51px',
       description:
-      '<p> primeiro projeto',
+        '<p> primeiro projeto',
       links: [
         {
           name: 'Primeiro projeto',
@@ -27,4 +33,10 @@ export class ProjectsComponent {
     }
   ])
 
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    })
+  }
 }
